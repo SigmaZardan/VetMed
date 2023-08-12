8a11bd8ae8d955a5ee8c1e9b7a26675765926318
Bibek Bhujel <biekbhujel077@gmail.com>
2023-08-12 22:06:57 +0545

    The color of the status bar and the navigation bar succefully handled and changed for darkmode

--- app/src/main/java/com/example/vetmed/feature_animal/presentation/AddAnimal.kt
+++ app/src/main/java/com/example/vetmed/feature_animal/presentation/AddAnimal.kt
@@ -1,16 +1,23 @@
 package com.example.vetmed.feature_animal.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddAnimal() {
-    Scaffold() {
+    Scaffold(
+        modifier = Modifier.background(MaterialTheme.colorScheme.surface).statusBarsPadding().navigationBarsPadding(),
+    ) {
         Text("This is the add animal screen ")
     }
 }
--- app/src/main/java/com/example/vetmed/feature_animal/presentation/AnimalScreen.kt
+++ app/src/main/java/com/example/vetmed/feature_animal/presentation/AnimalScreen.kt
@@ -1,14 +1,23 @@
 package com.example.vetmed.feature_animal.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.vetmed.R
 
@@ -21,19 +30,44 @@ fun AnimalScreen(
 ) {
 
     Scaffold(
+        modifier = Modifier.background(MaterialTheme.colorScheme.surface).statusBarsPadding().navigationBarsPadding(),
 
         topBar = {
-
+            AnimalTopBar()
         },
-        floatingActionButton =  {
+        content = {},
+        floatingActionButton = {
             FloatingActionButton(onClick = navigateToAddAnimalScreen) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(R.string.add_new_animal) )
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.add_new_animal),
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
             }
         }
+    )
 
-    ) {
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AnimalTopBar() {
+    TopAppBar(
+        title = {
+            Text("Animals")
+        },
+        actions = {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = stringResource(R.string.date_icon),
+                    tint = MaterialTheme.colorScheme.onSurface
+                )
 
-    }
+            }
+        }
+    )
 
 }
